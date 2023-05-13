c4f56fd9e247c51bb5aa840ed8fa31fe9af98994
poncho_boncho <Sorkolas@mail.ru>
2023-05-13 15:03:16 +0300

    added the ability to display images

--- app/src/main/java/poncho/boncho/affirmations/data/Datasource.kt
+++ app/src/main/java/poncho/boncho/affirmations/data/Datasource.kt
@@ -6,15 +6,15 @@ import poncho.boncho.affirmations.model.Affirmation
 class Datasource {
     fun loadAffirmations(): List<Affirmation>{
         return listOf<Affirmation>(
-            Affirmation(R.string.quote0),
-            Affirmation(R.string.quote1),
-            Affirmation(R.string.quote2),
-            Affirmation(R.string.quote3),
-            Affirmation(R.string.quote4),
-            Affirmation(R.string.quote5),
-            Affirmation(R.string.quote6),
-            Affirmation(R.string.quote7),
-            Affirmation(R.string.quote8),
+            Affirmation(R.string.quote0, R.drawable.image0),
+            Affirmation(R.string.quote1,R.drawable.image1),
+            Affirmation(R.string.quote2,R.drawable.image2),
+            Affirmation(R.string.quote3,R.drawable.image3),
+            Affirmation(R.string.quote4,R.drawable.image4),
+            Affirmation(R.string.quote5,R.drawable.image5),
+            Affirmation(R.string.quote6,R.drawable.image6),
+            Affirmation(R.string.quote7,R.drawable.image7),
+            Affirmation(R.string.quote8,R.drawable.image8),
         )
     }
 }
--- app/src/main/java/poncho/boncho/affirmations/model/Affirmation.kt
+++ app/src/main/java/poncho/boncho/affirmations/model/Affirmation.kt
@@ -1,5 +1,11 @@
 package poncho.boncho.affirmations.model
 
-data class Affirmation (val stringResourceId: Int) {
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class Affirmation (
+    @StringRes val stringResourceId: Int,
+    @DrawableRes val imageResourceId: Int
+    ) {
 
 }
